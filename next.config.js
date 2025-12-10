bed5f4f9375d331412f40efbccd2b24807c1b675
reganlai <reganlai@yahoo.com.hk>
2025-12-10 14:19:29 -0800

    m

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}'
-    },
-    '@mui/lab': {
-      transform: '@mui/lab/{{member}}'
-    }
-  },
+
   images: {
     remotePatterns: [
       {
